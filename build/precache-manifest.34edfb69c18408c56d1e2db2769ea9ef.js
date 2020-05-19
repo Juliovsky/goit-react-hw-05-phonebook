4fe6b74952e7763e0b51bb125459e83e8b14e91e
Yuliia <ybelova24@gmail.com>
2020-05-19 17:13:00 +0300

    second

---
+++ build/precache-manifest.34edfb69c18408c56d1e2db2769ea9ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c75c7422f9e7b1fab53ef069cf7e95e",
+    "url": "/Juliovsky.github.io/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "a412572968310308e726",
+    "url": "/Juliovsky.github.io/goit-react-hw-05-phonebook/static/css/main.dde318e7.chunk.css"
+  },
+  {
+    "revision": "6f372990c024015d17b6",
+    "url": "/Juliovsky.github.io/goit-react-hw-05-phonebook/static/js/2.07394a58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Juliovsky.github.io/goit-react-hw-05-phonebook/static/js/2.07394a58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a412572968310308e726",
+    "url": "/Juliovsky.github.io/goit-react-hw-05-phonebook/static/js/main.2a70dcfb.chunk.js"
+  },
+  {
+    "revision": "7633da49b4852203c212",
+    "url": "/Juliovsky.github.io/goit-react-hw-05-phonebook/static/js/runtime-main.b6d7bac9.js"
+  }
+]);
